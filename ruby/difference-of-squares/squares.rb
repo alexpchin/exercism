ec6763c7fbbadc6f6c782ad4660750334407456a
alexpchin <alex@womapp.com>
2014-11-28 13:10:32 +0000

    Reverting back as it's faster

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -2,23 +2,10 @@ class Squares
 
   attr_reader :sum_of_squares, :square_of_sums, :difference
 
-  def initialize(number)
-    @number         = number
-    @sum_of_squares = sum_of_squares
-    @square_of_sums = squares_of_sums
-    @difference     = difference
-  end
-
-  def sum_of_squares
-    (1..@number).reduce { |s, i| s += i**2 }
-  end
-
-  def squares_of_sums
-    (1..@number).reduce(:+)**2
-  end
-
-  def difference
-    (@sum_of_squares - @square_of_sums).abs
+  def initialize(n)
+    @sum_of_squares = (1..n).reduce { |s, i| s += i**2 }
+    @square_of_sums = (1..n).reduce(:+) ** 2
+    @difference     = (@sum_of_squares - @square_of_sums).abs
   end
 
 end
