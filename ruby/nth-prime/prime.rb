fdc28bd66f278d791b4a67291a5850a95e8546ea
alexpchin <alex@womapp.com>
2014-12-03 16:35:51 +0000

    Changing to use Math.sqrt(n) to reduce iterations.

--- ruby/nth-prime/prime.rb
+++ ruby/nth-prime/prime.rb
@@ -1,28 +1,34 @@
 class Prime
 
+  # Non-recursive solution
   def self.nth(n)
     raise ArgumentError if n < 1
-    @primes, @n, @i = [], n, 2
+    primes, n, i = [], n, 2
 
-    # Recursive soulution
-    # recursive(2)
-
-    # Non-recursive solution
-    while @primes.length != @n
-      @primes << @i if is_prime?(@i)
-      @i += 1
+    while primes.length != n
+      primes << i if is_prime?(i)
+      i += 1
     end
-    @primes[-1]
+    primes[-1]
   end
 
-  def self.recursive(i)
-    return @primes[-1] if @primes.length == @n
-    @primes << i if is_prime?(i)
-    recursive(i+1)
-  end
+  # # Recursive soulution (Stack level too deep)
+  # def self.nth(n)
+  #   raise ArgumentError if n < 1
+  #   @primes, @n = [], n
 
+  #   recursive(2)
+  # end
+
+  # def self.recursive(i)
+  #   return @primes[-1] if @primes.length == @n
+  #   @primes << i if is_prime?(i)
+  #   recursive(i+1)
+  # end
+
+  private
   def self.is_prime?(n)
-    !(2...n).to_a.any? { |d| n%d == 0 }
+    !(2..Math.sqrt(n)).to_a.any? { |d| n%d == 0 }
   end
 
 end
