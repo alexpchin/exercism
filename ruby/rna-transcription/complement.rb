fa91f8f9e375001f1a65daa105422e33ae3665dc
alexpchin <alex@womapp.com>
2014-12-06 00:31:18 +0000

    Further refactoring

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,8 +1,8 @@
 class Complement
 
-  [:of_rna, :of_dna].each do |v|
-    define_singleton_method v.to_sym do |x|
-      x.chars.map { |y| COMPLEMENTS[v][y] }.reduce(:+)
+  [:of_rna, :of_dna].each do |method_name|
+    define_singleton_method method_name do |x|
+      x.chars.map { |y| COMPLEMENTS[v][y.to_sym] }.reduce(:+)
     end
   end
 
@@ -10,50 +10,17 @@ class Complement
 
   COMPLEMENTS = {
     of_dna: {
-      "G" => "C",
-      "C" => "G",
-      "T" => "A",
-      "A" => "U"
+      :G => "C",
+      :C => "G",
+      :T => "A",
+      :A => "U"
     },
     of_rna: {
-      "C" => "G",
-      "G" => "C",
-      "A" => "T",
-      "U" => "A"
+      :C => "G",
+      :G => "C",
+      :A => "T",
+      :U => "A"
     },
   }
 
 end
-
-def self.of_dna(x)
-  x.chars.map { |y| COMPLEMENTS[y] }.reduce(:+)
-end
-
-def self.of_rna(x)
-  x.chars.map { |y| COMPLEMENTS.invert[y] }.reduce(:+)
-end
-
-
-# class Complement
-
-#   [:of_rna, :of_dna].each do |v|
-#     define_singleton_method v.to_sym do |x|
-#         @x = x
-#         a_method(Proc.new{ |y| v == :of_rna ? COMPLEMENTS.send(:key, y) : COMPLEMENTS.send(:fetch, y.to_sym)})
-#       end
-#   end
-
-#   def self.a_method(da_block)
-#     @x.split(//).map { |y| da_block.call(y).to_s }.reduce(:+)
-#   end
-  
-#   private
-
-#   COMPLEMENTS = {
-#     :G => "C",
-#     :C => "G",
-#     :T => "A",
-#     :A => "U"
-#   }
-
-# end
