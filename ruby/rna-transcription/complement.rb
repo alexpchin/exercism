3639bec34e87618b656114cf06f176083d65bc33
alexpchin <alex@womapp.com>
2014-12-06 00:47:20 +0000

    Just messing around

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -2,25 +2,18 @@ class Complement
 
   [:of_rna, :of_dna].each do |method_name|
     define_singleton_method method_name do |x|
-      x.chars.map { |y| COMPLEMENTS[v][y.to_sym] }.reduce(:+)
+      x.chars.map { |y| Complement.const_get(method_name.to_s.upcase)[y] }.reduce(:+)
     end
   end
 
   private
 
-  COMPLEMENTS = {
-    of_dna: {
-      :G => "C",
-      :C => "G",
-      :T => "A",
-      :A => "U"
-    },
-    of_rna: {
-      :C => "G",
-      :G => "C",
-      :A => "T",
-      :U => "A"
-    },
+  OF_DNA = {
+    "G" => "C",
+    "C" => "G",
+    "T" => "A",
+    "A" => "U"
   }
+  OF_RNA = OF_DNA.invert
 
 end
