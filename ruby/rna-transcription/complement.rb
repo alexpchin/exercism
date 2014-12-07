584514c3da74af39731cd165b13bd0471fb48724
alexpchin <alex@womapp.com>
2014-12-07 11:30:10 +0000

    Tests passing with tr

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,19 +1,27 @@
 class Complement
 
-  [:of_rna, :of_dna].each do |m|
-    define_singleton_method m do |x|
-      x.chars.map { |y| Complement.const_get(m.to_s.upcase)[y] }.reduce(:+)
-    end
+  def self.of_dna(dna)
+    dna.tr('ACGT', 'UGCA')
   end
+
+  def self.of_rna(rna)
+    rna.tr('UGCA', 'ACGT')
+  end
+
+  # [:of_rna, :of_dna].each do |m|
+  #   define_singleton_method m do |x|
+  #     x.chars.map { |y| Complement.const_get(m.to_s.upcase)[y] }.reduce(:+)
+  #   end
+  # end
 
   private
 
-  OF_DNA = {
-    "G" => "C",
-    "C" => "G",
-    "T" => "A",
-    "A" => "U"
-  }
-  OF_RNA = OF_DNA.invert
+  # OF_DNA = {
+  #   "G" => "C",
+  #   "C" => "G",
+  #   "T" => "A",
+  #   "A" => "U"
+  # }
+  # OF_RNA = OF_DNA.invert
 
 end
