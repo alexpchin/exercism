5e5eb2c141bf735b61962b01ba0650dfaa0dfa1a
alexpchin <alex@womapp.com>
2014-12-07 22:43:22 +0000

    Tests passing except multiple verses

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class FoodChainSong
 
   attr_reader :song
@@ -11,47 +13,42 @@ class FoodChainSong
   end
 
   def verses(x, y)
-puts "#{(x..y).to_a}\n\n"
-    verses = (x..y).to_a.map { |n| verse(n) }.join("hello")
-puts verses.inspect
-  verses
+    (x..y).to_a.map { |n| verse(n) }.join(SEPERATOR)
   end
 
   def verse(n)
     n -= 1
     @song << know(n)
-    @song << SEPERATOR if n < 8
+    @song << SEPERATOR
+    @song << unique(n) if n > 0
+    @song << SEPERATOR if n != 7
     swallow(n)
-    @song << die(n)
-
-puts "VERSE #{n}"
-puts @song.inspect
-puts "\n"
-
-    @song
+    @song << die(n) if n != 7
+    @song << SEPERATOR
   end
 
   def know(n)
-    [
-      "I know an old lady who swallowed a #{ANIMALS.keys[n].to_s}.",
-      ANIMALS.values[n]
-    ].compact.join(SEPERATOR)
+    "I know an old lady who swallowed a #{ANIMALS.keys[n].to_s}."
+  end
+
+  def unique(n)
+    ANIMALS.values[n]
   end
 
   def swallow(n)
     return if n == 0 || n == 7
     @song << "She swallowed the #{ANIMALS.keys[n].to_s} to catch the #{ANIMALS.keys[n-1].to_s}"
-    @song << " that wriggled and jiggled and tickled inside her" if ANIMALS.keys[n-1].to_s == "spider"
+    @song << " that wriggled and jiggled and tickled inside her" if (n-1) == 1
     @song << "." 
     @song << SEPERATOR
     swallow(n-1)
   end
 
   def die(n)
-    return if n > 7
-    "I don't know why she swallowed the #{ANIMALS.keys[0].to_s}. Perhaps she'll die.#{SEPERATOR}"
+    "I don't know why she swallowed the #{ANIMALS.keys[0].to_s}. Perhaps she'll die."
   end
 
+  private
   SEPERATOR = "\n"
 
   ANIMALS = {
--- ruby/food-chain/food_chain_test.rb
+++ ruby/food-chain/food_chain_test.rb
@@ -12,13 +12,13 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_fly
-    skip
+    # skip
     expected = "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n"
     assert_equal expected, song.verse(1)
   end
 
   def test_spider
-    skip
+    # skip
     expected = "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n" +
       "She swallowed the spider to catch the fly.\n" +
       "I don't know why she swallowed the fly. Perhaps she'll die.\n"
@@ -26,7 +26,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_bird
-    skip
+    # skip
     expected = "I know an old lady who swallowed a bird.\n" +
       "How absurd to swallow a bird!\n" +
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
@@ -36,7 +36,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_cat
-    skip
+    # skip
     expected = "I know an old lady who swallowed a cat.\n" +
       "Imagine that, to swallow a cat!\n" +
       "She swallowed the cat to catch the bird.\n" +
@@ -48,7 +48,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_dog
-    skip
+    # skip
     expected = "I know an old lady who swallowed a dog.\n" +
       "What a hog, to swallow a dog!\n" +
       "She swallowed the dog to catch the cat.\n" +
@@ -61,7 +61,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_goat
-    skip
+    # skip
     expected = "I know an old lady who swallowed a goat.\n" +
       "Just opened her throat and swallowed a goat!\n" +
       "She swallowed the goat to catch the dog.\n" +
@@ -75,7 +75,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_cow
-    skip
+    # skip
     expected = "I know an old lady who swallowed a cow.\n" +
       "I don't know how she swallowed a cow!\n" +
       "She swallowed the cow to catch the goat.\n" +
@@ -90,13 +90,13 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_horse
-    skip
-    expected = "I know an old lady who swallowed a horse.\n" +
-      "She's dead, of course!\n"
+    # skip
+    expected = "I know an old lady who swallowed a horse.\nShe's dead, of course!\n"
     assert_equal expected, song.verse(8)
   end
 
   def test_multiple_verses
+    # skip
     expected = ""
     expected << "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n\n"
     expected << "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n" +
@@ -106,7 +106,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_the_whole_song
-    skip
+    # skip
     assert_equal song.verses(1, 8), song.sing
   end
 end
