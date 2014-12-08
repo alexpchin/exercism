ba670b278ab7d14f68f9bd9cf73317751e37e4a8
alexpchin <alex@womapp.com>
2014-12-08 23:01:26 +0000

    Refactoring

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -8,11 +8,11 @@ class Sieve
   end
 
   def sieve_of_eratosthenes(n)
-    @array -= @array.map { |x| multiples(x, n) }.flatten.uniq
+    @array -= @array.map { |x| multiples(x, n) }.flatten
   end
 
   def multiples(x, n)
-    2.upto(n).map { |i| (x*i > n) ? nil : (x*i) }.compact
+    2.upto(n/x).map { |i| x*i }
   end
 
 end
