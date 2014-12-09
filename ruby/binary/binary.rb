9ad0064aa3b250de59e8e194ba310011cbf74bf4
alexpchin <alex@womapp.com>
2014-12-09 00:08:44 +0000

    Changed alphanumeric check to check if 1 or 0

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -7,12 +7,12 @@ class Binary
   end
 
   def convert(binary)
-    return 0 if has_alphanumeric_characters?(binary)
+    return 0 if has_incorrect_characters?(binary)
   end
 
   private
-  def has_alphanumeric_characters?(binary)
-    binary.respond_to?(:match) && binary.match(/[a-zA-Z]/)
+  def has_incorrect_characters?(binary)
+    binary.respond_to?(:match) && binary.match(/[^0-1]/)
   end
 
 end
