7decc2a2e6ddc987fd48ab78c2945e43a9d7e84a
alexpchin <alex@alexchin.co.uk>
2015-01-13 11:58:18 +0000

    Restructuring file to make it more manageable and easier to read

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,32 +1,53 @@
 class PhoneNumber
 
-  attr_accessor :string
+  attr_accessor :str
 
   def initialize(str)
-    @string = extract_number(str)
+    @str = str
+    @str = extract
+    @str = ERROR_RETURN unless valid?
   end
 
   def number
-    @string
+    str[-10..-1]
   end
 
   def area_code
-    @string[0..2]
+    str[0..2]
   end
 
   def to_s
-    PHONE_FORMAT.zip(@string.chars).compact.join
+    PHONE_FORMAT.zip(str[-10..-1].chars).compact.join
   end
 
   private
-  def extract_number(string)    
-    return ERROR_RETURN if string.scan(/[a-zA-Z]/).any?
-    string = string.scan(/\d+/).join
-    return string[-10..-1] if string.length == 10 || string.length == 11 && string[0] == '1'
-    return ERROR_RETURN
+  def extract
+    no_letters? ? str.scan(/\d+/).join : str
+  end
+
+  def valid?
+    no_letters? && ( ten_digits? || eleven_digits_and_starts_with_1? )
+  end
+
+  def no_letters?
+    str.scan(/[a-zA-Z]/).empty?
+  end
+
+  def ten_digits?
+    str.length == 10
+  end
+
+  def eleven_digits_and_starts_with_1?
+    str.length == 11 && str[0] == '1'
   end
 
   ERROR_RETURN = '0000000000'
   PHONE_FORMAT = ['(',nil,nil,') ',nil, nil, '-', nil, nil, nil, nil]
+
+  # def extract_number(str)    
+  #   str = str.scan(/\d+/).join
+  #   return str[-10..-1] if str.length == 10 || str.length == 11 && str[0] == '1'
+  #   return ERROR_RETURN
+  # end
 
 end
