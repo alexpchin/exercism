4fa308e1c7dccad189b9324787f60c2fba1ffa04
alexpchin <alex@alexchin.co.uk>
2015-01-13 11:15:14 +0000

    A bit of tidying up

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,13 +1,3 @@
-# - If the phone number is less than 10 digits assume that it is bad
-#   number
-# - If the phone number is 10 digits assume that it is good
-# - If the phone number is 11 digits and the first number is 1, trim the 1
-#   and use the last 10 digits
-# - If the phone number is 11 digits and the first number is not 1, then
-#   it is a bad number
-# - If the phone number is more than 11 digits assume that it is a bad
-#   number
-
 class PhoneNumber
 
   attr_accessor :string
@@ -29,18 +19,11 @@ class PhoneNumber
   end
 
   private
-  def extract_number(str)    
-    return ERROR_RETURN if str.chars.reject { |c| c if c[/\d+/] }.join.match(/[a-zA-Z]/)
-    str = str.chars.map { |c| c if c[/\d+/] }.compact.join
-    
-    if str.length == 10 
-      str 
-    elsif str.length == 11 && str[0] == '1'
-      str[1..-1]
-    else
-      ERROR_RETURN
-    end
-
+  def extract_number(string)    
+    return ERROR_RETURN if string.scan(/[a-zA-Z]/).any?
+    string = string.scan(/\d+/).join
+    return string[-10..-1] if string.length == 10 || string.length == 11 && string[0] == '1'
+    return ERROR_RETURN
   end
 
   ERROR_RETURN = '0000000000'
