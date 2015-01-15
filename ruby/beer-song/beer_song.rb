3ca232877ebce126060fce45844eab9bf08bdf8a
alexpchin <alex@alexchin.co.uk>
2015-01-15 17:13:39 +0000

    Updating dictionary

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -16,18 +16,26 @@ class BeerSong
   def self.create_song
     (TOTAL_NUMBER_OF_BOTTLES).downto(0).map do |n|
       if n > 0
-        "#{pluralize_bottle(n)}\s#{TEXT[6]}\s#{TEXT[1]},\s#{pluralize_bottle(n)}\s#{TEXT[6]}.\n#{TEXT[2]}\s#{pluralize_one(n)}\s#{TEXT[4]}\s#{pluralize_bottle(n-1)}\s#{TEXT[6]}\s#{TEXT[1]}.\n"
+        verse(n)
       else 
-        "#{pluralize_bottle(n).capitalize}\s#{TEXT[6]}\s#{TEXT[1]},\s#{pluralize_bottle(n)}\s#{TEXT[6]}.\n#{TEXT[5]}\s#{pluralize_bottle(TOTAL_NUMBER_OF_BOTTLES)}\s#{TEXT[6]}\s#{TEXT[1]}.\n"
+        last_verse(n)
       end
     end.reverse
   end
 
+  def self.last_verse(n)
+    "#{pluralize_bottle(n).capitalize}\s#{TEXT[6]}\s#{TEXT[1]},\s#{pluralize_bottle(n)}\s#{TEXT[6]}.\n#{TEXT[5]}\s#{pluralize_bottle(TOTAL_NUMBER_OF_BOTTLES)}\s#{TEXT[6]}\s#{TEXT[1]}.\n"
+  end
+
+  def self.verse(n)
+    "#{pluralize_bottle(n)}\s#{TEXT[6]}\s#{TEXT[1]},\s#{pluralize_bottle(n)}\s#{TEXT[6]}.\n#{TEXT[2]}\s#{pluralize_one(n)}\s#{TEXT[4]}\s#{pluralize_bottle(n-1)}\s#{TEXT[6]}\s#{TEXT[1]}.\n"
+  end
+
   def self.pluralize_bottle(n)
     case n
-    when 0 then "#{TEXT[3]}\s#{TEXT[7]}s"
+    when 0 then "#{TEXT[3]}\s#{TEXT[7]}#{TEXT[10]}"
     when 1 then "#{n}\s#{TEXT[7]}"
-    else "#{n}\s#{TEXT[7]}s"
+    else "#{n}\s#{TEXT[7]}#{TEXT[10]}"
     end
   end
 
@@ -46,10 +54,10 @@ class BeerSong
     6 => "of beer",
     7 => "bottle",
     8 => "one",
-    9 => "it"
+    9 => "it",
+    10 => "s"
   }
 
   SONG = self.create_song
-
 
 end
