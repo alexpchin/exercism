74eea4d9a6c94c821c8f8a88ffcb9d16f69958c4
alexpchin <alex@alexchin.co.uk>
2015-01-18 19:59:04 +0000

    Refactoring

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -2,8 +2,6 @@ class Clock
 
   MINUTES_IN_HOUR  = 60.to_f
   HOURS_IN_DAY     = 24
-  HOURS            = (0...HOURS_IN_DAY).to_a
-  SECONDS_MINUTES  = (0..MINUTES_IN_HOUR).to_a
   
   attr_accessor :hours, :minutes
 
@@ -18,32 +16,26 @@ class Clock
   end
 
   def +(n)
-    self.minutes = sort_by_time(SECONDS_MINUTES, self.minutes).cycle(2).to_a[self.minutes += n%MINUTES_IN_HOUR]
-    self.hours   = sort_by_time(HOURS, self.hours).cycle(2).to_a[self.hours += n/MINUTES_IN_HOUR]
-    normalize_24_hour_clock
+    self.hours   += n / MINUTES_IN_HOUR
+    self.minutes += n % MINUTES_IN_HOUR
+    normalize_hours
     self
   end
 
   def -(n)
-    # self.minutes = sort_by_time(SECONDS_MINUTES, self.minutes).cycle(2).to_a.reverse[self.minutes -= n%60]
-    # self.hours   = sort_by_time(HOURS, self.hours).cycle(2).to_a.reverse[self.hours -= n/60]
-    # normalize_24_hour_clock
-    self.minutes += n%MINUTES_IN_HOUR
-    self.hours -= (n/MINUTES_IN_HOUR)
-    self.hours += HOURS_IN_DAY if self.hours < 0
+    self.hours   -= n / MINUTES_IN_HOUR
+    self.minutes += n % MINUTES_IN_HOUR
+    normalize_hours
     self
   end
 
-  def normalize_24_hour_clock
-    self.hours -= self.hours if self.hours >= HOURS_IN_DAY
+  def normalize_hours
+    self.hours += HOURS_IN_DAY if self.hours < 0           # -1:00
+    self.hours -= self.hours if self.hours >= HOURS_IN_DAY # 24:00
   end
 
   def to_s
     "#{'%02d' % self.hours}:#{'%02d' % self.minutes}"
-  end
-
-  def sort_by_time(array, by)
-    array[0...by] + array[by..-1]
   end
 
   def ==(another_clock)
