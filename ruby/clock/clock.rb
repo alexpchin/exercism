764d87e0d79a31f50fe698aadc5ae8b7f9c19825
alexpchin <alex@alexchin.co.uk>
2015-01-19 10:05:40 +0000

    Redone in a different way but converting all hours to minutes

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -3,43 +3,30 @@ class Clock
   MINUTES_IN_HOUR  = 60.to_f
   HOURS_IN_DAY     = 24
   
-  attr_accessor :hours, :minutes
+  attr_accessor :minutes
 
-  def initialize(hours=0, minutes=0)
-    @hours   = hours
-    @minutes = minutes
-    self
+  def initialize(minutes=0)
+    @minutes = minutes % (HOURS_IN_DAY * MINUTES_IN_HOUR)
   end
 
   def self.at(hours, minutes=0)
-    new(hours, minutes)
+    new((hours * MINUTES_IN_HOUR) + minutes)
   end
 
   def +(n)
-    self.hours   += n / MINUTES_IN_HOUR
-    self.minutes += n % MINUTES_IN_HOUR
-    normalize_hours
-    self
+    Clock.new(self.minutes + n)
   end
 
   def -(n)
-    self.hours   -= n / MINUTES_IN_HOUR
-    self.minutes += n % MINUTES_IN_HOUR
-    normalize_hours
-    self
-  end
-
-  def normalize_hours
-    self.hours += HOURS_IN_DAY if self.hours < 0           # -1:00
-    self.hours -= self.hours if self.hours >= HOURS_IN_DAY # 24:00
+    self + -n
   end
 
   def to_s
-    "#{'%02d' % self.hours}:#{'%02d' % self.minutes}"
+    "%02d:%02d" % minutes.divmod(MINUTES_IN_HOUR)
   end
 
   def ==(another_clock)
-    self.hours == another_clock.hours && self.minutes == another_clock.minutes
+    self.minutes == another_clock.minutes
   end
 
 end
