f050f95082d4933bffae22d0fdc1b9ddea62e6b1
alexpchin <alex@alexchin.co.uk>
2015-01-19 09:52:03 +0000

    Refactoring a bit

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,13 +1,13 @@
 class Scrabble
 
   LETTERS = {
-    '1' => %w(A E I O U L N R S T),
-    '2' => %w(D G),
-    '3' => %w(B C M P),
-    '4' => %w(F H V W Y),
-    '5' => %w(K),
-    '8' => %w(J X),
-    '10' => %w(Q Z)
+    %w(A E I O U L N R S T) => 1,
+    %w(D G) => 2,
+    %w(B C M P) => 3,
+    %w(F H V W Y) => 4,
+    %w(K) => 5,
+    %w(J X) => 8,
+    %w(Q Z) => 10
   }
 
   attr_reader :letters
@@ -21,11 +21,18 @@ class Scrabble
   end
 
   def score
-    remove_whitespace.reduce(0) { |sum, letter| sum += LETTERS.select { |k,array| array.include?(letter.upcase) }.keys.reduce(:+).to_i }
+    remove_whitespace.reduce(0) do |sum, letter| 
+      sum += find_letter_value { letter }
+    end
   end
 
+  private
   def remove_whitespace
     letters.scan(/\w/)
+  end
+
+  def return_letter_value
+    LETTERS.select { |array,score| score.to_i if array.include?(yield.upcase) }.values.first
   end
 
 end
