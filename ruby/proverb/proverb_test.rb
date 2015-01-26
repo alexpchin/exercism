4a061188aafb21a24c5af23e0a0e542b8725b48e
alexpchin <alex@alexchin.co.uk>
2015-01-26 20:06:47 +0000

    All tests passing

--- ruby/proverb/proverb_test.rb
+++ ruby/proverb/proverb_test.rb
@@ -10,7 +10,6 @@ class ProverbTest < MiniTest::Unit::TestCase
   end
 
   def test_a_short_chain_of_consequences
-    skip
     proverb = Proverb.new('nail', 'shoe', 'horse')
     expected = "For want of a nail the shoe was lost.\n" \
       "For want of a shoe the horse was lost.\n" \
@@ -19,7 +18,6 @@ class ProverbTest < MiniTest::Unit::TestCase
   end
 
   def test_a_longer_chain_of_consequences
-    skip
     proverb = Proverb.new('nail', 'shoe', 'horse', 'rider')
     expected = "For want of a nail the shoe was lost.\n" \
       "For want of a shoe the horse was lost.\n" \
@@ -29,7 +27,6 @@ class ProverbTest < MiniTest::Unit::TestCase
   end
 
   def test_proverb_does_not_hard_code_the_rhyme_dictionary
-    skip
     proverb = Proverb.new('key', 'value')
     expected = "For want of a key the value was lost.\n" \
       'And all for the want of a key.'
@@ -37,7 +34,6 @@ class ProverbTest < MiniTest::Unit::TestCase
   end
 
   def test_the_whole_proverb # rubocop:disable Metrics/MethodLength
-    skip
     chain = %w(nail shoe horse rider message battle kingdom)
     proverb = Proverb.new(*chain)
     expected = "For want of a nail the shoe was lost.\n" \
@@ -52,7 +48,6 @@ class ProverbTest < MiniTest::Unit::TestCase
 
   # rubocop:disable Metrics/MethodLength
   def test_an_optional_qualifier_in_the_final_consequence
-    skip
     chain = %w(nail shoe horse rider message battle kingdom)
     proverb = Proverb.new(*chain, qualifier: 'horseshoe')
     expected = "For want of a nail the shoe was lost.\n" \
@@ -67,7 +62,6 @@ class ProverbTest < MiniTest::Unit::TestCase
   # rubocop:enable Metrics/MethodLength
 
   def test_proverb_is_same_each_time
-    skip
     proverb = Proverb.new('nail', 'shoe')
     assert_equal proverb.to_s, proverb.to_s
   end
