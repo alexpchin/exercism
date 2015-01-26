8a592e2608268610ba408703c86e1fa84361e954
alexpchin <alex@alexchin.co.uk>
2015-01-26 12:01:08 +0000

    Making variables more descriptive

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -6,21 +6,23 @@ class Anagram
     @word = word.downcase
   end
 
-  def match(array)
-    array.select { |w| w if is_anagram?(w) && !is_same_word?(w) }
+  def match(array_of_test_words)
+    array_of_test_words.select do |test_word| 
+      is_anagram?(test_word) && !is_same_word?(test_word)
+    end
   end
 
   private
-  def is_anagram?(w)
-    sort_by_char(word) == sort_by_char(w.downcase)
+  def is_anagram?(test_word)
+    sort_by_char(word) == sort_by_char(test_word.downcase)
   end
 
-  def sort_by_char(w)
-    w.chars.sort.join
+  def sort_by_char(test_word)
+    test_word.chars.sort.join
   end
 
-  def is_same_word?(w)
-    word == w.downcase
+  def is_same_word?(test_word)
+    word == test_word.downcase
   end
 
 end
