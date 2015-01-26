0d5dff2337735a52cb8b6c97b4081ead33073a2c
alexpchin <alex@alexchin.co.uk>
2015-01-26 11:57:04 +0000

    Test passing, using new function to setup string and number of verses

--- ruby/house/house.rb
+++ ruby/house/house.rb
@@ -1,30 +1,36 @@
 class House
 
-  def self.recite
-    number_of_verses = VERBS_AND_OBJECTS.keys.count
-    @song = ""
+  def initialize 
+    @number_of_verses = VERBS_AND_OBJECTS.keys.count
+    @song = String.new
+  end
 
-    0.upto(number_of_verses-1) do |verse|
-      self.this(verse)
+  def self.recite
+    new.song_builder
+  end
+
+  def song_builder
+    0.upto(@number_of_verses-1) do |verse|
+      this(verse)
 
       (verse-1).downto(0) do |verse|
-        self.that(verse)
+        that(verse)
       end
-      
+
       @song << "\n"
     end
-    
+
     @song.strip << "\n"
   end
 
   private
-  def self.this(verse)
+  def this(verse)
     keys = VERBS_AND_OBJECTS.keys
     @song << "This is the #{VERBS_AND_OBJECTS[keys[verse]]}"
     @song << "\n"
   end
 
-  def self.that(verse)
+  def that(verse)
     keys   = VERBS_AND_OBJECTS.keys
     @song << "that #{keys[verse].to_s.gsub('_', ' ')} the #{VERBS_AND_OBJECTS[keys[verse]]}"
     @song << "\n"
@@ -46,93 +52,3 @@ class House
   }
 
 end
-
-# This is the house that Jack built.
-
-# This is the malt
-# that lay in the house that Jack built.
-
-# This is the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the dog
-# that worried the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the cow with the crumpled horn
-# that tossed the dog
-# that worried the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the maiden all forlorn
-# that milked the cow with the crumpled horn
-# that tossed the dog
-# that worried the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the man all tattered and torn
-# that kissed the maiden all forlorn
-# that milked the cow with the crumpled horn
-# that tossed the dog
-# that worried the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the priest all shaven and shorn
-# that married the man all tattered and torn
-# that kissed the maiden all forlorn
-# that milked the cow with the crumpled horn
-# that tossed the dog
-# that worried the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the rooster that crowed in the morn
-# that woke the priest all shaven and shorn
-# that married the man all tattered and torn
-# that kissed the maiden all forlorn
-# that milked the cow with the crumpled horn
-# that tossed the dog
-# that worried the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the farmer sowing his corn
-# that kept the rooster that crowed in the morn
-# that woke the priest all shaven and shorn
-# that married the man all tattered and torn
-# that kissed the maiden all forlorn
-# that milked the cow with the crumpled horn
-# that tossed the dog
-# that worried the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
-
-# This is the horse and the hound and the horn
-# that belonged to the farmer sowing his corn
-# that kept the rooster that crowed in the morn
-# that woke the priest all shaven and shorn
-# that married the man all tattered and torn
-# that kissed the maiden all forlorn
-# that milked the cow with the crumpled horn
-# that tossed the dog
-# that worried the cat
-# that killed the rat
-# that ate the malt
-# that lay in the house that Jack built.
