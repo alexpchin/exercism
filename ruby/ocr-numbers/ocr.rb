5fd888603eadc179f00e4575e44b035c2a64b49a
alexpchin <alex@alexchin.co.uk>
2015-01-27 18:20:24 +0000

    Adding some code that doesn't work

--- ruby/ocr-numbers/ocr.rb
+++ ruby/ocr-numbers/ocr.rb
@@ -1,16 +1,9 @@
 class OCR
 
-  NUMBERS = [
-    ["9", "\s\_\n\|\_\|\n\s\_\|\n"],
-    ["8", "\s\_\n\|\_\|\n\|\_\|\n"],
-    ["7", "\s\_\n\s\s\|\n\s\s\|\n"],
-    ["6", "\s\_\n\|\_\n\|\_\|\n"],
-    ["5", "\s\_\n\|\_\n\s\_\|\n"],
-    ["4", "\n\|\_\|\n\s\s\|\n"],
-    ["3", "\s\_\n\s\_\|\n\s\_\|\n"],
-    ["2", "\s\_\n\s\_\|\n\|\_\n"],
-    ["1", "\n\s\s\|\n\s\s\|\n"],
-    ["0", "\s\_\n\|\s\|\n\|\_\|\n"],
+  NUMBERS_TRANSPOSED = [
+    [" _",  "", " _", " _",  "",  " _", " _", " _"," _", " _" ],
+    ["| |"," |"," _|"," _|","|_|","|_", "|_", " |","|_|","|_|"],
+    ["|_|"," |","|_", " _|", " |"," _|","|_|"," |","|_|"," _|"]
   ]
 
   def initialize(text)
@@ -18,85 +11,108 @@ class OCR
   end
 
   def convert
-      text = <<-NUMBER.chomp
-      _
-    || |
-    ||_|
-
-      NUMBER
-
-    conversion = @text
-
-    NUMBERS.each do |pair|
-      conversion.gsub!(pair[1].strip, pair[0])
-    end 
-  
     p " "
-    p text.chars
-    p "Text: #{@text.chars.transpose}"
-    p "Conversion: #{conversion}"
+    p to_lines
+    p split_on_double_pipes
+    p split_on_triple_spaces
+    p "*****************************************"
+    
+    # NUMBERS_TRANSPOSED.map do |line|
+    #   split_on_triple_spaces.map do |char_part|
+    #     p char_part
+    #     line.index(char_part)
+    #   end
+    # end
+  end
 
-    conversion
+  private
+  def to_lines
+    @text = @text.split(/\n+/)
+  end
+
+  # "  ||  " => "  |", "|  "
+  def split_on_double_pipes
+    @text = @text.map do |line| 
+      line.gsub("||", "|\n|").split(/\n+/)
+    end.flatten
+  end
+
+  # "   |" => "  ", " |"
+  def split_on_triple_spaces
+    @text = @text.map do |line| 
+      line.gsub("\s\s\s\s", "\s\s*\s\s").split(/\*/)
+    end.flatten
   end
 
 end
 
-# def glue(sum, index)
-#   if index == 10
-#     sum << "?"
-#   else 
-#     sum << index.to_s
+# NUMBERS_TRANSPOSED = [
+#   ["\s\_",  "",      "\s\_",  "\s\_",  "",      "\s\_",  "\s\_",  "\s\_",  "\s\_",  "\s\_"  ],
+#   ["\|\s\|","\s\s\|","\s\_\|","\s\_\|","\|\_\|","\|\_",  "\|\_",  "\s\s\|","\|\_\|","\|\_\|"],
+#   ["\|\_\|","\s\s\|","\|\_",  "\s\_\|","\s\s\|","\s\_\|","\|\_\|","\s\s\|","\|\_\|","\s\_\|"]
+# ]
+
+# NUMBERS = [
+#   ["9", "\s\_\n\|\_\|\n\s\_\|\n"],
+#   ["8", "\s\_\n\|\_\|\n\|\_\|\n"],
+#   ["7", "\s\_\n\s\s\|\n\s\s\|\n"],
+#   ["6", "\s\_\n\|\_\n\|\_\|\n"],
+#   ["5", "\s\_\n\|\_\n\s\_\|\n"],
+#   ["4", "\n\|\_\|\n\s\s\|\n"],
+#   ["3", "\s\_\n\s\_\|\n\s\_\|\n"],
+#   ["2", "\s\_\n\s\_\|\n\|\_\n"],
+#   ["1", "\n\s\s\|\n\s\s\|\n"],
+#   ["0", "\s\_\n\|\s\|\n\|\_\|\n"],
+# ]
+
+# NUMBERS_TRANSPOSED = [
+#   ["\s\_\n",  "\n",      "\s\_\n",  "\s\_\n",  "\n",      "\s\_\n",  "\s\_\n",  "\s\_\n",  "\s\_\n",  "\s\_\n"  ],
+#   ["\|\s\|\n","\s\s\|\n","\s\_\|\n","\s\_\|\n","\|\_\|\n","\|\_\n",  "\|\_\n",  "\s\s\|\n","\|\_\|\n","\|\_\|\n"],
+#   ["\|\_\|\n","\s\s\|\n","\|\_\n",  "\s\_\|\n","\s\s\|\n","\s\_\|\n","\|\_\|\n","\s\s\|\n","\|\_\|\n","\s\_\|\n"]
+# ]
+
+# line.split(/[\s\s]\s+/)
+# line.split(/[\s\s]\s+/)
+# line.scan(/[^\s\s]*/)
+# line.split(/(?<=\s\s\s)/)
+
+# @text = @text.map do |line| 
+#   line.gsub("\s\s", "\s\n\s").split(/\n+/)
+# end
+
+# private
+# def to_lines
+#   @text.lines
+# end
+
+# def convert
+#   p " "
+#   to_lines.map do |char_part_in_line|
+#     # a = char_part_in_line.split("\n").map { |char_part| char_part.gsub("\s\s", "\s") }
+#     a = char_part_in_line.map { |char_part| char_part.gsub("\s\s", "\s") }
+#     p a.inspect
+#     NUMBERS_TRANSPOSED.index(a)
 #   end
 # end
 
-# NUMBERS.each.with_index.reduce("") do |sum, (value, index)|
-#   @text == value ? glue(sum, index) : sum
-# end  
-
-# conversion.delete!("\n")
-# conversion.delete!("\s")
-# p NUMBERS.sort.reverse
-
-# NUMBERS.sort.reverse_each.with_index do |value, index| 
-#   conversion.gsub!(/#{Regexp.escape(value)}/, index.to_s)
+# conversion = @text.dup
+# @conversion = ""
+# NUMBERS.each do |pair|
+#   if conversion.include?(pair[1])
+#     conversion.gsub!(pair[1], "") 
+#     @conversion << pair[0]
+#   end
 # end
-# conversion
+# @conversion
 
-# NUMBERS = [
-#   "\_\|\|\|\_\|",   # 0
-#   "\|\|",           # 1
-#   "\_\_\|\|\_",     # 2
-#   "\_\_\|\_\|",     # 3
-#   "\|\_\|\|",       # 4
-#   "\_\|\_\_\|",     # 5
-#   "\_\|\_\|\_\|",   # 6
-#   "\_\|\|",         # 7
-#   "\_\|\_\|\|\_\|", # 8
-#   "\_\|\_\|\_\|"    # 9
-# ]
+# @result = []
+# NUMBERS_TRANSPOSED.each do |line|
+#   to_lines.each do |char_part_in_line|
+#     line.map.with_index do |char_part, index|
+#       @result << index if char_part_in_line.gsub(/\n|\s/, "") == char_part.gsub(/\n|\s/, "")
+#     end
+#   end
+# end
+# @result
 
-# NUMBERS = [
-#   "\s\_\|\s\|\|\_\|", # 0
-#   "\s\s\|\s\s\|",     # 1
-#   "\s\_\s\_\|\|\_",   # 2
-#   "\s\_\s\_\|\s\_\|", # 3
-#   "\|\_\|\s\s\|",     # 4
-#   "\s\_\|\_\s\_\|",   # 5
-#   "\s\_\|\_\|\_\|",   # 6
-#   "\s\_\s\s\|\s\s\|", # 7
-#   "\s\_\|\_\|\|\_\|", # 8
-#   "\s\_\|\_\|\s\_\|"  # 9
-# ]
-
-# NUMBERS = [
-#   "\s\_\n\|\s\|\n\|\_\|\n", # 0
-#   "\n\s\s\|\n\s\s\|\n",     # 1
-#   "\s\_\n\s\_\|\n\|\_\n",   # 2
-#   "\s\_\n\s\_\|\n\s\_\|\n", # 3
-#   "\n\|\_\|\n\s\s\|\n",     # 4
-#   "\s\_\n\|\_\n\s\_\|\n",   # 5
-#   "\s\_\n\|\_\n\|\_\|\n",   # 6
-#   "\s\_\n\s\s\|\n\s\s\|\n", # 7
-#   "\s\_\n\|\_\|\n\|\_\|\n", # 8
-#   "\s\_\n\|\_\|\n\s\_\|\n"  # 9
-# ]
+# p [char_part_in_line.gsub(/\n|\s/, ""), char_part.gsub(/\n|\s/, "")]
