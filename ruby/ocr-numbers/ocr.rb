6c72b408946d1fdf73a51e7da57a792f224bbfae
alexpchin <alex@alexchin.co.uk>
2015-02-04 11:04:02 +0000

    Adding completed solution OCR

--- ruby/ocr-numbers/ocr.rb
+++ ruby/ocr-numbers/ocr.rb
@@ -1,88 +1,44 @@
-class Array
-  def safe_transpose
-    result = []
-    max_size = self.max { |a,b| a.size <=> b.size }.size
-    max_size.times do |i|
-      result[i] = Array.new(self.first.size)
-      self.each_with_index { |r,j| result[i][j] = r[i] }
-    end
-    result
-  end
-end
-
 class OCR
-
   NUMBERS = {
-    :" _\n| |\n|_|\n" => "0",
-    :" _\n| |\n|_|\n" => "0",
-    :"\n  |\n  |\n"   => "1",
-    :" _\n _|\n|_\n"  => "2",
-    :" _\n _|\n _|\n" => "3",
-    :"\n|_|\n  |\n"   => "4",
-    :" _\n|_\n _|\n"  => "5",
-    :" _\n|_\n|_|\n"  => "6",
-    :" _\n  |\n  |\n" => "7",
-    :" _\n|_|\n|_|\n" => "8",
-    :" _\n|_|\n _|\n" => "9"
+    :" _\n| |\n|_|" => '0',
+    :"\n  |\n  |"   => '1',
+    :" _\n _|\n|_"  => '2',
+    :" _\n _|\n _|" => '3',
+    :"\n|_|\n  |"   => '4',
+    :" _\n|_\n _|"  => '5',
+    :" _\n|_\n|_|"  => '6',
+    :" _\n  |\n  |" => '7',
+    :" _\n|_|\n|_|" => '8',
+    :" _\n|_|\n _|" => '9'
   }
   NUMBERS.default = "?"
-
-  FULL_NUMBERS = {
-    :"   " => ",",
-    :" _ \n| |\n|_|\n" => "0",
-    :"   \n  |\n  |\n" => "1",
-    :" _ \n _|\n|_ \n" => "2",
-    :" _ \n _|\n _|\n" => "3",
-    :"   \n|_|\n  |\n" => "4",
-    :" _ \n|_ \n _|\n" => "5",
-    :" _ \n|_ \n|_|\n" => "6",
-    :" _ \n  |\n  |\n" => "7",
-    :" _ \n|_|\n|_|\n" => "8",
-    :" _ \n|_|\n _|\n" => "9"
-  }
-  FULL_NUMBERS.default = "?"  
-
-  MAX_NUMBER_LENGTH = NUMBERS.keys.max.length
 
   attr_reader :text
 
   def initialize(text)
-    @text =  text
+    @text = text
   end
 
   def convert
-    one_number? ? convert_one_number : convert_multiple_numbers
+    text.split(/(?<=\n)\n/).map do |text_row|
+      convert_row(text_row)
+    end.join(',')
   end
 
   private
-  def convert_one_number
-    NUMBERS[text.to_sym]
-  end
 
-  def convert_multiple_numbers
-    digits.map do |digit|
-      NUMBERS[digit.to_sym] == NUMBERS.default ? FULL_NUMBERS[digit.to_sym] : NUMBERS[digit.to_sym]
-    end.join
-  end
-
-  def lines
-    text.split("\n")
-  end
-
-  def three_characters
-    lines.map do |line|
-      line.scan(/...|../)
+  def convert_row(text_row)
+    text_numbers(text_row).each_with_object('') do |text_number, number|
+      number << NUMBERS[text_number.to_sym]
     end
   end
 
-  def digits
-    three_characters.safe_transpose.map do |character|
-      character.compact.join("\n") << "\n"
+  def text_numbers(text_row)
+    text_row.each_line.map do |line|
+      line.scan(/(.{3}\n|.{3}|.*\n)/)
+    end.transpose.map do |lines|
+      lines.flatten.map(&:rstrip).join("\n")
     end
   end
-
-  def one_number?
-    text.length <= MAX_NUMBER_LENGTH
-  end
-
+  
 end
