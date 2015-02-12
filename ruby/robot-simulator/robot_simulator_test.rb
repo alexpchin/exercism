dc0e060d5ac443d0d860a9b7f63d713c420100f0
alexpchin <alex@alexchin.co.uk>
2015-02-12 13:07:20 +0000

    All tests passing

--- ruby/robot-simulator/robot_simulator_test.rb
+++ ruby/robot-simulator/robot_simulator_test.rb
@@ -16,82 +16,70 @@ class RobotTurningTest < MiniTest::Unit::TestCase
   end
 
   def test_invalid_robot_bearing
-    skip
     assert_raises ArgumentError do
       robot.orient(:crood)
     end
   end
 
   def test_turn_right_from_north
-    skip
     robot.orient(:north)
     robot.turn_right
     assert_equal :east, robot.bearing
   end
 
   def test_turn_right_from_east
-    skip
     robot.orient(:east)
     robot.turn_right
     assert_equal :south, robot.bearing
   end
 
   def test_turn_right_from_south
-    skip
     robot.orient(:south)
     robot.turn_right
     assert_equal :west, robot.bearing
   end
 
   def test_turn_right_from_west
-    skip
     robot.orient(:west)
     robot.turn_right
     assert_equal :north, robot.bearing
   end
 
   def test_turn_left_from_north
-    skip
     robot.orient(:north)
     robot.turn_left
     assert_equal :west, robot.bearing
   end
 
   def test_turn_left_from_east
-    skip
     robot.orient(:east)
     robot.turn_left
     assert_equal :north, robot.bearing
   end
 
   def test_turn_left_from_south
-    skip
     robot.orient(:south)
     robot.turn_left
     assert_equal :east, robot.bearing
   end
 
   def test_turn_left_from_west
-    skip
     robot.orient(:west)
     robot.turn_left
     assert_equal :south, robot.bearing
   end
 
   def test_robot_coordinates
-    skip
     robot.at(3, 0)
     assert_equal [3, 0], robot.coordinates
   end
 
   def test_other_robot_coordinates
-    skip
     robot.at(-2, 5)
     assert_equal [-2, 5], robot.coordinates
   end
 
   def test_advance_when_facing_north
-    skip
     robot.at(0, 0)
     robot.orient(:north)
     robot.advance
@@ -99,7 +87,6 @@ class RobotTurningTest < MiniTest::Unit::TestCase
   end
 
   def test_advance_when_facing_east
-    skip
     robot.at(0, 0)
     robot.orient(:east)
     robot.advance
@@ -107,7 +94,6 @@ class RobotTurningTest < MiniTest::Unit::TestCase
   end
 
   def test_advance_when_facing_south
-    skip
     robot.at(0, 0)
     robot.orient(:south)
     robot.advance
@@ -115,7 +101,6 @@ class RobotTurningTest < MiniTest::Unit::TestCase
   end
 
   def test_advance_when_facing_west
-    skip
     robot.at(0, 0)
     robot.orient(:west)
     robot.advance
@@ -129,28 +114,23 @@ class RobotSimulatorTest < MiniTest::Unit::TestCase
   end
 
   def test_instructions_for_turning_left
-    skip
     assert_equal [:turn_left], simulator.instructions('L')
   end
 
   def test_instructions_for_turning_right
-    skip
     assert_equal [:turn_right], simulator.instructions('R')
   end
 
   def test_instructions_for_advancing
-    skip
     assert_equal [:advance], simulator.instructions('A')
   end
 
   def test_series_of_instructions
-    skip
     commands = [:turn_right, :advance, :advance, :turn_left]
     assert_equal commands, simulator.instructions('RAAL')
   end
 
   def test_instruct_robot
-    skip
     robot = Robot.new
     simulator.place(robot, x: -2, y: 1, direction: :east)
     simulator.evaluate(robot, 'RLAALAL')
@@ -160,7 +140,6 @@ class RobotSimulatorTest < MiniTest::Unit::TestCase
 
   # rubocop:disable Metrics/AbcSize
   def test_instruct_many_robots # rubocop:disable Metrics/MethodLength
-    skip
     robot1 = Robot.new
     robot2 = Robot.new
     robot3 = Robot.new
