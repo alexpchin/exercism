6ece18789660d7297a129cdb480b0250a6e6d368
alexpchin <alex@alexchin.co.uk>
2015-06-19 20:03:10 +0100

    Added bob.js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,11 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  return "Whatever.";
 };
+
+Bob.nothing = function() {
+
+}
 
 module.exports = Bob;
