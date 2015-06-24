07601d5b62f5d82838b42365696b7e62887989cd
alexpchin <alex@alexchin.co.uk>
2015-06-24 15:07:02 +0200

    Prototype solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,31 +1,29 @@
-// Alternative solution
+// Prototype solution
 
-var Bob = function() {
+var Bob = function() {};
 
-  this.hey = function(input) {
-    if (isSilent(input)) {
-      return 'Fine. Be that way!';
-    } else if (isShouting(input)) {
-      return 'Whoa, chill out!';
-    } else if (isQuestion(input)) {
-      return 'Sure.';
-    } else {
-      return 'Whatever.';
-    }
-  }
+function isSilent(input) {
+  return /^\s*$/.test(input);
+}
 
-  function isSilent(input) {
-      return /^\s*$/.test(input);
-  }
+function isQuestion(input) {
+  return /\?$/.test(input);
+}
 
-  function isQuestion(input) {
-      return /\?$/.test(input);
-  }
+function isShouting(input) {
+  return input === input.toUpperCase() && /[A-Z]/.test(input);
+}
 
-  function isShouting(input) {
-      return input === input.toUpperCase() && /[A-Z]/.test(input);
+Bob.prototype.hey = function(input) {
+  if (isSilent(input)) {
+    return 'Fine. Be that way!';
+  } else if (isShouting(input)) {
+    return 'Whoa, chill out!';
+  } else if (isQuestion(input)) {
+    return 'Sure.';
+  } else {
+    return 'Whatever.';
   }
 };
-
 
 module.exports = Bob;
