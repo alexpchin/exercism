800247b65686294a86c3cf2ede7f412d4dca5a09
alexpchin <alex@alexchin.co.uk>
2015-06-24 13:16:14 +0200

    Bob Javascript all tests passing

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,11 +1,45 @@
 var Bob = function() {};
 
+Bob.prototype.responses = {
+  nothing: "Fine. Be that way!",
+  yell: "Whoa, chill out!",
+  question: "Sure.",
+  other: "Whatever."
+}
+
 Bob.prototype.hey = function(input) {
-  return "Whatever.";
-};
+  this.input = input.replace(/[0-9\t ]/, '')
+  if (this.nothing()) {
+    return this.responses.nothing;
+  } else if (this.yell()) {
+    return this.responses.yell;
+  } else if (this.question()) {
+    return this.responses.question;
+  } else {
+    return this.responses.other; 
+  }
+}
 
-Bob.nothing = function() {
+Bob.prototype.nothing = function() {
+  return this.input === null || this.input === "" || this.input .trim() == 0;
+}
 
+Bob.prototype.yell = function() {
+  var letters = this.letters();
+  if (letters) {
+    return letters == letters.toUpperCase();
+  }
+}
+
+Bob.prototype.question = function() {
+  return this.input.slice(-1) == "?";
+}
+
+Bob.prototype.letters = function() {
+  var letters = this.input.match(/[a-zA-ZäöüÄÖÜß]+/);
+  if (letters != null) {
+    return letters[0];
+  }
 }
 
 module.exports = Bob;
