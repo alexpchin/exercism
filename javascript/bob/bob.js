dfbfccd608ebf5a159009785e08781633056dc2b
alexpchin <alex@alexchin.co.uk>
2015-06-24 15:06:21 +0200

    Alternative solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,31 +1,31 @@
-// Prototype solution
+// Alternative solution
 
-var isSilent = function(input){
- return (input.length<1);
+var Bob = function() {
+
+  this.hey = function(input) {
+    if (isSilent(input)) {
+      return 'Fine. Be that way!';
+    } else if (isShouting(input)) {
+      return 'Whoa, chill out!';
+    } else if (isQuestion(input)) {
+      return 'Sure.';
+    } else {
+      return 'Whatever.';
+    }
+  }
+
+  function isSilent(input) {
+      return /^\s*$/.test(input);
+  }
+
+  function isQuestion(input) {
+      return /\?$/.test(input);
+  }
+
+  function isShouting(input) {
+      return input === input.toUpperCase() && /[A-Z]/.test(input);
+  }
 };
 
-var isShouting = function(input) {
- return input.toUpperCase() === input && input.match(/[a-zA-Z]/);
-};
 
-var isQuestion = function(input){
- return !isSilent(input) ? input.charAt(input.length-1)==='?' : false;
-};
-
-var Bob = module.exports = function() {};
-
-Bob.prototype.hey = function(input) {
- input = (typeof input === 'string' ? input.trim() : '');
-
- if(isSilent(input)){
-   return 'Fine. Be that way!';
- }
- if (isShouting(input)){
-   return 'Whoa, chill out!';
- }
- if (isQuestion(input)){
-   return 'Sure.';
- }
- return 'Whatever.';
-
-};
+module.exports = Bob;
