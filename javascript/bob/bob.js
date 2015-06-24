b61bcebc720f49c641c1560293a7e9cb7657333c
alexpchin <alex@alexchin.co.uk>
2015-06-24 15:07:38 +0200

    Tidying

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,28 +1,29 @@
-// Prototype solution
+// Alternative solution
 
-var Bob = function() {};
+var Bob = function() {
 
-function isSilent(input) {
-  return /^\s*$/.test(input);
-}
+  this.hey = function(input) {
+    if (isSilent(input)) {
+      return 'Fine. Be that way!';
+    } else if (isShouting(input)) {
+      return 'Whoa, chill out!';
+    } else if (isQuestion(input)) {
+      return 'Sure.';
+    } else {
+      return 'Whatever.';
+    }
+  }
 
-function isQuestion(input) {
-  return /\?$/.test(input);
-}
+  function isSilent(input) {
+    return /^\s*$/.test(input);
+  }
 
-function isShouting(input) {
-  return input === input.toUpperCase() && /[A-Z]/.test(input);
-}
+  function isQuestion(input) {
+    return /\?$/.test(input);
+  }
 
-Bob.prototype.hey = function(input) {
-  if (isSilent(input)) {
-    return 'Fine. Be that way!';
-  } else if (isShouting(input)) {
-    return 'Whoa, chill out!';
-  } else if (isQuestion(input)) {
-    return 'Sure.';
-  } else {
-    return 'Whatever.';
+  function isShouting(input) {
+    return input === input.toUpperCase() && /[A-Z]/.test(input);
   }
 };
 
