093999488adc89befb8657f1dc8bbf963d51db8d
alexpchin <alex@alexchin.co.uk>
2015-06-24 15:13:48 +0200

    Constructor solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,28 +1,25 @@
-// Alternative solution
+// Constructor solution
 
 var Bob = function() {
 
   this.hey = function(input) {
-    if (isSilent(input)) {
+    if (this.isSilent(input)) {
       return 'Fine. Be that way!';
-    } else if (isShouting(input)) {
+    } else if (this.isShouting(input)) {
       return 'Whoa, chill out!';
-    } else if (isQuestion(input)) {
+    } else if (this.isQuestion(input)) {
       return 'Sure.';
     } else {
       return 'Whatever.';
     }
-  }
-
-  function isSilent(input) {
+  },
+  this.isSilent = function(input) {
     return /^\s*$/.test(input);
-  }
-
-  function isQuestion(input) {
+  },
+  this.isQuestion = function(input) {
     return /\?$/.test(input);
-  }
-
-  function isShouting(input) {
+  },
+  this.isShouting = function(input) {
     return input === input.toUpperCase() && /[A-Z]/.test(input);
   }
 };
--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
